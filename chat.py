4e9c3ed650b452e03d8f4611f77fe842640c5bdf
erika-fonseca <erika-fonseca>
2024-03-26 16:30:08 +0000

    thumbs up down

---
+++ chat.py
@@ -0,0 +1,6 @@
+from streamlit_feedback import streamlit_feedback
+feedback = streamlit_feedback(
+    feedback_type="thumbs",
+    optional_text_label="[Optional] Please provide an explanation",
+)
+feedback
